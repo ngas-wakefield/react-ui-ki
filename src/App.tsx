1fc2041d928022a9e2415169b266e9fd3f0d3c30
Ngas Wakefield <ngaswakefield@gmail.com>
2025-06-07 19:31:33 +1200

    set up layout and base light, dark theme

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Footer from './layout/Footer/Footer';
+import Header from './layout/Header/Header';
+import Layout from './layout/Layout';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Layout />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
